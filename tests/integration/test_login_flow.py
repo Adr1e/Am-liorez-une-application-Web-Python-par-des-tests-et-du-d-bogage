acda30f59c21b4a13f29a71fce8fc345e31b7556
Adr1e <adrienfischer274@gmail.com>
2025-11-07 12:15:05 +0100

    chore: remove README from qa branch

--- tests/integration/test_login_flow.py
+++ tests/integration/test_login_flow.py
@@ -9,3 +9,9 @@ def test_login_valid_email(client):
 def test_login_invalid_email(client):
     resp = client.post("/showSummary", data={"email": "unknown@mail.com"})
     assert resp.status_code in (302, 303)
+
+def test_points_displayed_on_welcome_page(client, mock_club, mock_competitions):
+    """Check that the user's points are displayed on the welcome page."""
+    response = client.post("/showSummary", data={"email": mock_club["email"]})
+    assert response.status_code == 200
+    assert str(mock_club["points"]) in response.text
